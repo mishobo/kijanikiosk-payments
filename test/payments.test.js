aee80a463e25d5f1a82f8b43bf9edba182202548
mishobo <husseinabdallah287@gmail.com>
2026-07-19 18:04:20 +0300

    Revert "fault-injection: wrong expected total in calculateTotal test (Test fault)"
    This reverts commit 900cb5e77f40e7094289f7892def74538a5544ed.

--- test/payments.test.js
+++ test/payments.test.js
@@ -9,7 +9,7 @@ test('calculateTotal sums price * quantity across items', () => {
     { price: 10, quantity: 2 },
     { price: 5, quantity: 3 },
   ];
-  assert.equal(calculateTotal(items), 999999);
+  assert.equal(calculateTotal(items), 35);
 });
 
 test('calculateTotal throws on non-array input', () => {
