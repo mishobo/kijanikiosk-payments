e48c12432e3402c609fcdf707bf551c7561493b2
mishobo <husseinabdallah287@gmail.com>
2026-07-19 17:53:46 +0300

    fault-injection: remove closing brace in calculateTotal (Lint fault)

--- src/payments.js
+++ src/payments.js
@@ -5,7 +5,7 @@ function calculateTotal(items) {
     throw new TypeError('Items must be an array');
   }
   return items.reduce((sum, item) => sum + item.price * item.quantity, 0);
-}
+
 
 function formatCurrency(amount, currency = 'KES') {
   return `${currency} ${amount.toFixed(2)}`;
